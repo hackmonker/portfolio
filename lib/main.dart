ee7082fb211d0c7107e3184ea41f351d1505609d
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2022-10-22 21:37:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MaterialChildState extends State<MaterialChild> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shuvojyoti',
+      title: 'Paul',
       theme: theme.themeLight,
       darkTheme: theme.themeDark,
       themeMode: widget.provider.themeMode,
