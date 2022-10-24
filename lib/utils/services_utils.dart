4631e8101ffba605f1bb9ef947cc7dfb5100d24a
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2022-10-24 11:48:01 +0530

    services test

--- lib/utils/services_utils.dart
+++ lib/utils/services_utils.dart
@@ -10,18 +10,18 @@ class ServicesUtils {
   ];
 
   static const List<String> servicesTitles = [
-    "Mobile App Development",
-    "UI/UX Designing",
-    "Rapid Prototyping",
-    "Technical Blog Writing",
+    "Pharmacy",
+    "Linux",
+    "Docker",
+    "Servers",
     "Open Source - GitHub",
   ];
 
   static const List<String> servicesDescription = [
-    "Android app development via Flutter\n- Splash Screen\n- Firebase Auth/Cloud\n- REST APIs\n- Maps integration and more...!",
-    "Modern UI/UX Designing\n- Adobe XD\n- Mobile & Web designs\n- Interactive UI designs\n- Responsiveness\n- Promo Videos and more..!",
-    "Rapid Prototype via Flutter\n- Working MVP\n- Quick & Working prototype",
-    "Technical Blog writing\n- Medium blogs\n- Soothing header images\n- SEO friendly\n- Researched topics and more..!",
+    "Pharmaceutical Science\n- Pharmacology\n- Pharmascognosy\n- Medicinal Chemistry\n- Pharmaceutics",
+    "Linux Development\n- Bash Scripting\n- Automation\n- Arch Linux\n- Linux Kali...and more!",
+    "Dockers\n- Using docker containers daily\n- Automation using dockers\n- Docker compose...and more!",
+    "Server and Automation\n- Linux servers\n- Home Servers using rpi\n- Docker containers in servers\n- Ansible automation and more..!",
     "Open source GitHub Projects\n- Awesome README.md\n- Well documented\n- Header images and more...!",
   ];
 }
