cee837558ddd64335d403bfcdbf6fed983f5b524
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2022-10-24 12:31:08 +0530

    test

--- lib/sections/services/widgets/_services_card.dart
+++ lib/sections/services/widgets/_services_card.dart
@@ -102,7 +102,15 @@ class _ServiceCardState extends State<_ServiceCard> {
               Image.asset(
                 widget.serviceIcon,
                 height: AppDimensions.normalize(30),
-                color: widget.serviceIcon.contains(StaticUtils.openSource, StaticUtils.appDev) &&
+                color: widget.serviceIcon.contains(StaticUtils.openSource) &&
+                        !appProvider.isDark
+                    ? Colors.black
+                    : null,
+              ),
+              Image.asset(
+                widget.serviceIcon,
+                height: AppDimensions.normalize(30),
+                color: widget.serviceIcon.contains(StaticUtils.appDev) &&
                         !appProvider.isDark
                     ? Colors.black
                     : null,
