5f142d526c812c089fd06307922d0f73b3b6e1aa
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2024-08-12 18:48:05 +0530

    Update about_utils.dart

--- lib/utils/about_utils.dart
+++ lib/utils/about_utils.dart
@@ -1,7 +1,7 @@
 class AboutUtils {
   static const String aboutMeHeadline =
-      'I\'m Shuvojyoti Paul, a Pharmaceutical student and a python lover.';
+      'I\'m Shuvojyoti Paul, a MBA Student, Pharmacist and a python lover.';
 
   static const String aboutMeDetail =
-      'I\'m a B.Pharm student studying pharmacy from Girijananda University, Assam. I have experience in softwares and programming. Any projects that interest me is a learning experience for me. I love and know python as well as linux, along with docker, bash and a bit of C++';
+      'I\'m a MBA student studying Management from Institute of Rural Management, Anand (IRMA). I have experience in softwares and programming. Any projects that interest me is a learning experience for me. I love and know python as well as linux, along with docker, bash and a bit of C++';
 }
